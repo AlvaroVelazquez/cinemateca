1eca47d8b461d7cc8497f89cd1c0d388b135ba92
AlvaroVelazquez <alvarovelazquez12345@gmail.com>
2019-03-01 09:33:47 +0100

    vistiquis de quiliqui

--- models/actor.py
+++ models/actor.py
@@ -2,7 +2,6 @@ from odoo import models,fields, api
 
 class actor(models.Model):
     _inherit = 'base.entidad'
-    _name = 'cinemateca.actor'
     name = fields.Char(string="name", required=True, help="Nombre del actor")
     rol = fields.Char(string="rol", help="Nombre del rol")
     cache = fields.Integer(string="cache")
--- models/salaCine.py
+++ models/salaCine.py
@@ -1,9 +1,8 @@
 from odoo import models,fields, api
 
 class salaCine(models.Model):
-    #Clasica
+    #Extension
     _inherit = 'base.empresa'
-    _name = 'cinemateca.salaCine'
     pelicula_id = fields.Many2one("cinemateca.pelicula", string="pelicula")
     sesion_ids = fields.One2many("cinemateca.sesion", "salaCine_id", string="sesion")
     
