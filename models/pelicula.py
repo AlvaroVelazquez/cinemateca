6a7014396ffb23e12cb360f74580fa7e95d51190
AlvaroVelazquez <alvarovelazquez12345@gmail.com>
2019-03-01 20:12:44 +0100

    Ya tengo todo, solo me queda pulir

--- models/pelicula.py
+++ models/pelicula.py
@@ -3,7 +3,10 @@ from odoo import models,fields, api
 class pelicula(models.Model):
     _name = 'cinemateca.pelicula'
     title = fields.Char(string="name", required=True, help="Nombre de la  pelicula")
-    asistencia = fields.Integer(string="asistencia", help="numero de espectadores")
+    genero = fields.Selection([
+        ('Accion', 'Accion'),
+        ('Aventura', 'Aventura'), 
+        ('Misterio', 'Misterio')], string='Genero')
     salaCine_ids = fields.One2many("base.empresa", "pelicula_id", string="sala")
     director_id = fields.Many2one("cinemateca.director", string="director")
     actor_ids = fields.One2many("base.entidad", "pelicula_id", string="actor")
