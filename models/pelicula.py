33753b402429b2fac91ff7e6113069e7661af8fa
AlvaroVelazquez <alvarovelazquez12345@gmail.com>
2019-03-01 19:21:45 +0100

    api.depends error identation

--- models/pelicula.py
+++ models/pelicula.py
@@ -3,13 +3,11 @@ from odoo import models,fields, api
 class pelicula(models.Model):
     _name = 'cinemateca.pelicula'
     title = fields.Char(string="name", required=True, help="Nombre de la  pelicula")
-    recaudacion = fields.Integer(string="recaudacion", help="recaudacion", compute="sumar")
+    asistencia = fields.Integer(string="asistencia", help="numero de espectadores")
     salaCine_ids = fields.One2many("base.empresa", "pelicula_id", string="sala")
     director_id = fields.Many2one("cinemateca.director", string="director")
     actor_ids = fields.One2many("base.entidad", "pelicula_id", string="actor")
     sesion_ids = fields.One2many("cinemateca.sesion", "pelicula_id", string="sesion")
 
 
-@api.depends('totalRecaudacion')
-    def sumar(self):
-        self.recaudacion = sesion.x
+
--- models/sesion.py
+++ models/sesion.py
@@ -5,14 +5,16 @@ class sesion(models.Model):
     _inherit = 'base.empresa'
     _name = 'cinemateca.sesion'
     precio = fields.Integer(string="precio", help="precio de la pelicula")
-    asistencia = fields.Integer(string="asistencia", help="numero de espectadores", compute="guardar")
+    asistencia = fields.Integer(string="asistencia", help="numero de espectadores")
+    recaudacion = fields.Integer(string="recaudacion", help="recaudacion", compute="sumar")
     pelicula_id = fields.Many2one("cinemateca.pelicula", string="pelicula")
     salaCine_id = fields.Many2one("base.empresa", string="pelicula")
 
 
-   @api.depends('guardarValores')
-    def guardar(self):
-        x = self.precio * self.asistencia
+@api.depends('totalRecaudacion')
+    def sumar(self):
+        self.recaudacion = self.asistencia * self.precio
+
 
             
 
